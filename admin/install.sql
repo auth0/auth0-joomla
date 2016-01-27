46ab4826fffd27746ad46ed29bea74adb8f42ced
German Lena <german.lena@gmail.com>
2016-01-27 17:36:30 -0300

    fix table creation

--- admin/install.sql
+++ admin/install.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE IF NOT EXISTS `#__auth0_joomla_connect` (
   `joomla_userid` int(15) NOT NULL,
-  `auth0_userid` bigint(20) unsigned NOT NULL,
+  `auth0_userid` varchar(30) NOT NULL,
   `joined_date` int(15) NOT NULL,
   `linked` smallint(4) NOT NULL,
   PRIMARY KEY (`joomla_userid`)
